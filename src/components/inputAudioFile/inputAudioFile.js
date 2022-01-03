ea1c6733fc73cd2540287d7bb302f0432f3e100c
kir-dev13 <kirill87rabota@gmail.com>
2022-01-04 01:25:42 +0300

    init input

---
+++ src/components/inputAudioFile/inputAudioFile.js
@@ -0,0 +1,13 @@
+const InputAudioFile = () => {
+    return (
+        <input
+            id="input"
+            type="file"
+            onChange={() => {
+                // loadFile();
+            }}
+        />
+    );
+};
+
+export default InputAudioFile;
