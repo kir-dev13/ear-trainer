8eefb7ac6291a2c3fe376755688bf0d88982f176
kir-dev13 <kirill87rabota@gmail.com>
2022-01-05 16:47:03 +0300

    replace checkInputFiles in InputAudioFile component

--- src/App.js
+++ src/App.js
@@ -6,34 +6,10 @@ import "./App.sass";
 function App() {
     const [tracks, setTracks] = useState([]);
 
-    function checkInputFiles(data, dataType) {
-        const resultFiles = [];
-        for (let file of data) {
-            if (resultFiles.length < 5) {
-                if (dataType.test(file.type)) {
-                    resultFiles.push(file);
-                } else {
-                    console.log("не аудио!");
-                }
-            } else {
-                console.log("попытались загрузить больше файлов");
-                return resultFiles;
-            }
-        }
-        return resultFiles;
-    }
-
-    function loadAudioFiles(e) {
-        const audioFiles = checkInputFiles(
-            e.target.files,
-            /audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/
-        );
-
+    function loadAudioFiles(audioFiles) {
         audioFiles.length > 0
             ? setTracks([...audioFiles])
             : console.log("ни одного аудио файла не было загружено");
-
-        e.target.value = "";
     }
 
     return (
--- src/components/inputAudioFile/inputAudioFile.js
+++ src/components/inputAudioFile/inputAudioFile.js
@@ -2,6 +2,25 @@
 
 import "./inputAudioFile.sass";
 
+function checkInputFiles(data, dataType, e = null) {
+    const resultFiles = [];
+    for (let file of data) {
+        if (resultFiles.length < 5) {
+            if (dataType.test(file.type)) {
+                resultFiles.push(file);
+            } else {
+                console.log("не аудио!");
+            }
+        } else {
+            console.log("попытались загрузить больше файлов");
+            e ? (e.target.value = "") : console.log(" объекта события нет");
+            return resultFiles;
+        }
+    }
+    e ? (e.target.value = "") : console.log(" объекта события нет");
+    return resultFiles;
+}
+
 const InputAudioFile = (props) => {
     return (
         <form action="">
@@ -10,7 +29,13 @@ const InputAudioFile = (props) => {
                 type="file"
                 multiple
                 onChange={(e) => {
-                    props.loadAudioFiles(e);
+                    props.loadAudioFiles(
+                        checkInputFiles(
+                            e.target.files,
+                            /audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/
+                        ),
+                        e
+                    );
                 }}
             />
             <label htmlFor="input">label</label>
