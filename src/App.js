5c85b282aa985dc9be3847b1eff3dbfd82298842
kir-dev13 <kirill87rabota@gmail.com>
2022-01-05 16:11:28 +0300

    fix loadFiles - old files will replace by new files

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import "./App.sass";
 function App() {
     const [tracks, setTracks] = useState([]);
 
-    function checkInputFiles(data, finalData, dataType) {
+    function checkInputFiles(data, dataType) {
         const resultFiles = [];
         for (let file of data) {
-            if (finalData.length + resultFiles.length < 5) {
+            if (resultFiles.length < 5) {
                 if (dataType.test(file.type)) {
                     resultFiles.push(file);
                 } else {
@@ -17,36 +17,20 @@ function App() {
                 }
             } else {
                 console.log("попытались загрузить больше файлов");
+                return resultFiles;
             }
         }
         return resultFiles;
     }
 
     function loadAudioFiles(e) {
-        const audioType =
-            /audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/;
-        const audioFiles = checkInputFiles(e.target.files, tracks, audioType);
-        // const files = [];
-        // console.log("загруженные файлы: ", e.target.files);
-        // for (let file of e.target.files) {
-        //     if (tracks.length + files.length < 5) {
-        //         //когда 4, то добавляется ещё один, пятый, file
-        //         if (
-        //             /audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/.test(
-        //                 file.type
-        //             )
-        //         ) {
-        //             files.push(file);
-        //         } else {
-        //             console.log("не аудио!");
-        //         }
-        //     } else {
-        //         console.log("попытались загрузить больше файлов или не аудио");
-        //     }
-        // }
+        const audioFiles = checkInputFiles(
+            e.target.files,
+            /audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/
+        );
 
         audioFiles.length > 0
-            ? setTracks([...tracks, ...audioFiles])
+            ? setTracks([...audioFiles])
             : console.log("ни одного аудио файла не было загружено");
 
         e.target.value = "";
@@ -57,9 +41,15 @@ function App() {
             <InputAudioFile loadAudioFiles={loadAudioFiles} />
             <button
                 onClick={() => {
-                    // playMusic();
                     console.log(tracks);
                     console.log(tracks.length);
+                }}
+            >
+                Console
+            </button>
+            <button
+                onClick={() => {
+                    // playMusic();
                 }}
             >
                 Play
