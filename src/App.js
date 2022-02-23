9f7b02992b7254c8a1aa7979e82f7c294168c878
kir-dev13 <kirill87rabota@gmail.com>
2022-02-23 22:40:47 +0300

    fix loadBlob file

--- src/App.js
+++ src/App.js
@@ -8,54 +8,65 @@ import "./App.css";
 import "./App.sass";
 
 function App() {
-    const [tracks, setTracks] = useState([]);
+    const [track, setTracks] = useState(null);
+    const [wave, setWave] = useState(null);
 
     const wavesurferRef = useRef();
 
-    const loadInWaveSurf = (wavesurfer) => {
-        if (wavesurfer) {
-            console.log(wavesurfer);
-            if (tracks[0]) {
-                console.log(tracks[0]);
-                wavesurfer.loadBlob(tracks[0]);
-            }
-            // else {
-            //     wavesurfer.load("/dance2bass.mp3");
-            // }
+    // console.log("tadaa", track);
 
-            wavesurfer.on("ready", () => {
-                console.log("WaveSurfer is ready");
-                wavesurfer.playPause();
-            });
-        }
-    };
+    // const loadInWaveSurf = (wavesurfer) => {
+    //     if (wavesurfer) {
+    //         if (track) {
+    //             console.log("трэк: ", track);
+    //             console.log(wavesurfer);
+    //             wavesurfer.loadBlob(track);
+    //         }
+    //         // else {
+    //         // wavesurfer.load("/dance2bass.mp3");
+    //         // }
+    //         console.log(wavesurfer);
+
+    //         wavesurfer.on("ready", () => {
+    //             console.log("WaveSurfer is ready");
+    //             // wavesurfer.playPause();
+    //         });
+    //     }
+    // };
 
     const handleWSMount = (waveSurfer) => {
-        console.log("rerender");
+        console.log("render");
 
         wavesurferRef.current = waveSurfer;
+        setWave(wavesurferRef.current);
 
-        loadInWaveSurf(wavesurferRef.current);
+        // loadInWaveSurf(wavesurferRef.current);
     };
 
     function loadAudioFiles(audioFiles, e) {
         if (audioFiles.length > 0) {
-            console.log(audioFiles);
-            setTracks([...audioFiles]);
+            setTracks(audioFiles[0]);
         } else {
             console.log("ни одного аудио файла не было загружено");
         }
         e.target.value = "";
     }
+    console.log("мы увидели");
+
+    useEffect(() => {
+        if (track) {
+            wave.loadBlob(track);
+        }
+    }, [track]);
 
     return (
         <div className="App">
             <InputAudioFile
-                trackName={tracks[0]?.name}
+                trackName={track?.name}
                 loadAudioFiles={loadAudioFiles}
             />
 
-            {/* <Player selectedTrack={tracks[0]} /> */}
+            {/* <Player selectedTrack={tracks} /> */}
             <WaveSurfer onMount={handleWSMount}>
                 <WaveForm id="waveform"></WaveForm>
                 <div id="timeline" />
