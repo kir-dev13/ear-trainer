b46b45aa7a73c4e8943e5a07f2f05addd3c395f9
kir-dev13 <kirill87rabota@gmail.com>
2022-02-23 21:48:39 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+import { WaveSurfer, WaveForm } from "wavesurfer-react";
+
 import Player from "./components/Player/Player";
 
 import InputAudioFile from "./components/inputAudioFile/inputAudioFile";
@@ -8,8 +10,37 @@ import "./App.sass";
 function App() {
     const [tracks, setTracks] = useState([]);
 
+    const wavesurferRef = useRef();
+
+    const loadInWaveSurf = (wavesurfer) => {
+        if (wavesurfer) {
+            console.log(wavesurfer);
+            if (tracks[0]) {
+                console.log(tracks[0]);
+                wavesurfer.loadBlob(tracks[0]);
+            }
+            // else {
+            //     wavesurfer.load("/dance2bass.mp3");
+            // }
+
+            wavesurfer.on("ready", () => {
+                console.log("WaveSurfer is ready");
+                wavesurfer.playPause();
+            });
+        }
+    };
+
+    const handleWSMount = (waveSurfer) => {
+        console.log("rerender");
+
+        wavesurferRef.current = waveSurfer;
+
+        loadInWaveSurf(wavesurferRef.current);
+    };
+
     function loadAudioFiles(audioFiles, e) {
         if (audioFiles.length > 0) {
+            console.log(audioFiles);
             setTracks([...audioFiles]);
         } else {
             console.log("ни одного аудио файла не было загружено");
@@ -24,7 +55,11 @@ function App() {
                 loadAudioFiles={loadAudioFiles}
             />
 
-            <Player selectedTrack={tracks[0]} />
+            {/* <Player selectedTrack={tracks[0]} /> */}
+            <WaveSurfer onMount={handleWSMount}>
+                <WaveForm id="waveform"></WaveForm>
+                <div id="timeline" />
+            </WaveSurfer>
         </div>
     );
 }
