13c20f40b0619057c34279f95b2e8d5f631df002
kir-dev13 <kirill87rabota@gmail.com>
2022-03-07 15:00:04 +0300

    restore sideFunctions

--- src/logic/sideFunctions.js
+++ src/logic/sideFunctions.js
@@ -2,4 +2,16 @@ const delay = (ms) => {
     return new Promise((resolve) => setTimeout(() => resolve()), ms);
 };
 
-export { delay };
+const delayWithInterval = (ms, func) => {
+    return new Promise((resolve) => {
+        let timerId = setInterval(() => {
+            func(ms);
+            ms = ms - 1000;
+        }, 1000);
+        delay(ms)
+            .then(() => clearInterval(timerId))
+            .then(() => delay(1000).then(() => resolve()));
+    });
+};
+
+// const delayWithInterval = (ms, func) => {//     let timerId = setInterval(() => {//         func(ms);//         ms = ms - 1000;//     }, 1000);//     delay(ms).then(() => clearInterval(timerId));// };\r
